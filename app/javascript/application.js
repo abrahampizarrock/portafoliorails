a32aa06b4232aeac131c4168d30bd474102154e9
abrahampizarrock <128652700+abrahampizarrock@users.noreply.github.com>
2023-07-07 13:50:52 -0400

    codigo rails casi terminado solo faltan detalles.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import './mijs.js';
 import "@hotwired/turbo-rails"
 import "controllers"
---
+++ app/javascript/mijs.js
@@ -0,0 +1,9 @@
+
+function toggleText(textId1, textId2) {
+    var text1 = document.getElementById(textId1);
+    var text2 = document.getElementById(textId2);
+  
+    text1.style.display = "none";
+    text2.style.display = "block";
+  }
+  
